3bc4e2269b765b20e3d27faaeab8c557bad79d5f
naimulislamshakil <ms0413354@gmail.com>
2022-05-10 22:09:43 +0600

    header section complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Component/Page/About/About";
+import Appointment from "./Component/Page/Appointment/Appointment";
+import Contact from "./Component/Page/Contact/Contact";
+import Home from "./Component/Page/Home/Home";
+import Login from "./Component/Page/Login/Login";
+import Review from "./Component/Page/Review/Review";
+import Header from "./Component/Shered/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<About />} />
+        <Route path="review" element={<Review />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="appointment" element={<Appointment />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Component/Page/Home/Banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+import chair from "../../../assets/images/chair.png";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="hero min-h-screen bg-img">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
+        <div className="mr-10">
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. <br /> In deleniti eaque aut
+            repudiandae et a id nisi.
+          </p>
+          <button className="btn btn-primary font-bold uppercase text-white bg-gradient-to-r from-secondary to-primary ">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
