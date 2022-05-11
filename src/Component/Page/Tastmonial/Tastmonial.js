edfce7c9a2293d63818107f6112110ef78f6bd39
naimulislamshakil <ms0413354@gmail.com>
2022-05-11 20:13:04 +0600

    create Tastmonial Card Section

--- src/Component/Page/Tastmonial/Tastmonial.js
+++ src/Component/Page/Tastmonial/Tastmonial.js
@@ -1,10 +1,39 @@
 import React from "react";
 import quare from "../../../assets/icons/quote.svg";
+import person1 from "../../../assets/images/people1.png";
+import person2 from "../../../assets/images/people2.png";
+import person3 from "../../../assets/images/people3.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
 
 const Tastmonial = () => {
+  const reviews = [
+    {
+      id: 1,
+      name: "Winson Herry",
+      location: "Califonia",
+      review: "",
+      img: person1,
+      slied: "",
+    },
+    {
+      id: 1,
+      name: "",
+      location: "",
+      review: "",
+      img: person2,
+      slied: "",
+    },
+    {
+      id: 1,
+      name: "",
+      location: "",
+      review: "",
+      img: person3,
+      slied: "",
+    },
+  ];
   return (
     <section className="mx-10">
       <div className="flex justify-between">
---
+++ src/Component/Page/Tastmonial/TastmonialCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TastmonialCard = () => {
+  return (
+    <div class="card w-96 text-primary-content">
+      <div class="card-body">
+        <h2 class="card-title">Card title!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TastmonialCard;
