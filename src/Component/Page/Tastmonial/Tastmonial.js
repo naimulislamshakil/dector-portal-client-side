170652cfd6f35026794a80084ea49c2b96d8d42c
naimulislamshakil <ms0413354@gmail.com>
2022-05-11 22:15:14 +0600

    Tastmonial section complete

--- src/Component/Page/Tastmonial/Tastmonial.js
+++ src/Component/Page/Tastmonial/Tastmonial.js
@@ -3,6 +3,7 @@ import quare from "../../../assets/icons/quote.svg";
 import person1 from "../../../assets/images/people1.png";
 import person2 from "../../../assets/images/people2.png";
 import person3 from "../../../assets/images/people3.png";
+import TastmonialCard from "../../Page/Tastmonial/TastmonialCard";
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
@@ -13,25 +14,28 @@ const Tastmonial = () => {
       id: 1,
       name: "Winson Herry",
       location: "Califonia",
-      review: "",
+      review:
+        "Director Nelson weaves an intricate, entertaining, socially relevant and extremely likable film in his latest film.. Doctor. A host of excellent artistes and a magical musical score by Aniruddh adds to the charm.",
       img: person1,
-      slied: "",
+      slied: "fade-up",
     },
     {
-      id: 1,
-      name: "",
-      location: "",
-      review: "",
+      id: 2,
+      name: "Richard N Nees",
+      location: "New York",
+      review:
+        "Siva Karthikeyan is the biggest highlight of the film as he showcases a different side to his personality. He as an army doctor evokes good comedy, drama, and carries the film on his shoulder. ",
       img: person2,
-      slied: "",
+      slied: "fade-down",
     },
     {
-      id: 1,
-      name: "",
-      location: "",
-      review: "",
+      id: 3,
+      name: "Tina D McClure",
+      location: "Arizona",
+      review:
+        "Well scripted Dark Humour Comedy. When it comes to the comedy genre, we shouldn't expect the logics. So I strongly say there was no lacks. Siva and Vinay character designed very well.",
       img: person3,
-      slied: "",
+      slied: "fade-up-right",
     },
   ];
   return (
@@ -47,7 +51,11 @@ const Tastmonial = () => {
           <img className="w-3/4" src={quare} alt="" />
         </div>
       </div>
-      <div></div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 my-10">
+        {reviews.map((review) => (
+          <TastmonialCard key={review.id} review={review}></TastmonialCard>
+        ))}
+      </div>
     </section>
   );
 };
--- src/Component/Page/Tastmonial/TastmonialCard.js
+++ src/Component/Page/Tastmonial/TastmonialCard.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-const TastmonialCard = () => {
+const TastmonialCard = ({ review }) => {
   return (
-    <div class="card w-96 text-primary-content">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn">Buy Now</button>
+    <div data-aos={review.slied}>
+      <div class="card w-96 text-primary-content shadow-2xl transition ease-in-out delay-150  hover:translate-y-6  hover:scale-100  duration-300">
+        <div class="card-body">
+          <p>{review.review}</p>
+          <div className="flex mt-3 items-center">
+            <div class="avatar">
+              <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                <img src={review.img} alt="" />
+              </div>
+            </div>
+            <div className="">
+              <h2>{review.name}</h2>
+              <p>{review.location}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
