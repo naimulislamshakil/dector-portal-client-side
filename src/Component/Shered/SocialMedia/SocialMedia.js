effc934658dd2ca67a3e0c21ba89c4bbf15b0282
naimulislamshakil <ms0413354@gmail.com>
2022-05-14 06:22:35 +0600

    Social media login system

--- src/Component/Shered/SocialMedia/SocialMedia.js
+++ src/Component/Shered/SocialMedia/SocialMedia.js
@@ -2,22 +2,43 @@ import React from "react";
 import fb from "../../../assets/icons/facebook.png";
 import google from "../../../assets/icons/google.png";
 import github from "../../../assets/icons/github (1).png";
+import auth from "../../../firebase.config";
+import {
+  useSignInWithFacebook,
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import Loading from "../Loading/Loading";
 
 const SocialMedia = () => {
+  const [signInWithFacebook, user, loading] = useSignInWithFacebook(auth);
+  const [signInWithGithub, user1, loading1] = useSignInWithGithub(auth);
+  const [signInWithGoogle, user2, loading2] = useSignInWithGoogle(auth);
+  const navigation = useNavigate();
+
+  if (user || user1 || user2) {
+    navigation("/");
+  }
+
+  if (loading || loading1 || loading2) {
+    return <Loading></Loading>;
+  }
+
   return (
     <div>
       <div className="flex justify-evenly">
-        <div class="avatar">
+        <div onClick={() => signInWithFacebook()} class="avatar">
           <div class="w-14 mask mask-hexagon">
             <img src={fb} alt="" />
           </div>
         </div>
-        <div class="avatar">
+        <div onClick={() => signInWithGoogle()} class="avatar">
           <div class="w-14 mask mask-hexagon">
             <img src={google} alt="" />
           </div>
         </div>
-        <div class="avatar">
+        <div onClick={() => signInWithGithub()} class="avatar">
           <div class="w-14 mask mask-hexagon">
             <img src={github} alt="" />
           </div>
